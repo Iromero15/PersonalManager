e841a6895f1cddc5dc84a89d7ff7b904056abaa2
Iromero15 <ignacion.romero@outlook.es>
2026-03-06 18:02:00 -0300

    arreglo sobre agregar datos

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    // Esto es como darle permisos de 'write' al struct en memoria
+    protected $fillable = [
+        'name',
+        'user_id'
+    ];
 }
